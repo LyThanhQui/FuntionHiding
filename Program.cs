25a2af231b8c3308f48d18ac3f2d1bb5c796d67b
Qui Ly <lquiitfuture@gmail.com>
2019-12-15 23:07:30 +0700

    add project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FuntionHiding
+{
+    class Class1
+    {
+        public void display()
+        {
+            Console.WriteLine("Super class display method");
+        }
+    }
+    class Class2 : Class1
+    {
+        public new void display()
+        {
+            Console.WriteLine("Sub class display method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 obj = new Class2();
+            obj.display();
+            Console.ReadKey();
+        }
+    }
+
+}
